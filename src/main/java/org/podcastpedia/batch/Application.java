611cc70480a888547bf96135f4729c4a3418e91b
amacoder <ama@codingpedia.org>
2014-08-14 08:48:00 +0200

    added job name as parameter for the application

--- src/main/java/org/podcastpedia/batch/Application.java
+++ src/main/java/org/podcastpedia/batch/Application.java
@@ -1,6 +1,5 @@
 package org.podcastpedia.batch;
 
-import java.util.Arrays;
 import java.util.Date;
 
 import org.apache.commons.logging.Log;
@@ -25,20 +24,23 @@ import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
 @EnableAutoConfiguration
-public class ApplicationWithJobLauncher {
+public class Application {
 
-    public static void main(String[] args) throws BeansException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException, InterruptedException {
+    private static final String NEW_EPISODES_NOTIFICATION_JOB = "newEpisodesNotificationJob";
+	private static final String ADD_NEW_PODCAST_JOB = "addNewPodcastJob";
+
+	public static void main(String[] args) throws BeansException, JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException, InterruptedException {
     	
-    	Log log = LogFactory.getLog(ApplicationWithJobLauncher.class);
+    	Log log = LogFactory.getLog(Application.class);
     	    	
-        SpringApplication app = new SpringApplication(ApplicationWithJobLauncher.class);
+        SpringApplication app = new SpringApplication(Application.class);
         app.setWebEnvironment(false);
         ConfigurableApplicationContext ctx= app.run(args);
         JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
-    	
-        if("1".equals(args[0])){
+    	        		
+        if(ADD_NEW_PODCAST_JOB.equals(args[0])){
         	//addNewPodcastJob
-        	Job addNewPodcastJob = ctx.getBean("addNewPodcastJob", Job.class);
+        	Job addNewPodcastJob = ctx.getBean(ADD_NEW_PODCAST_JOB, Job.class);
         	JobParameters jobParameters = new JobParametersBuilder()
     		.addDate("date", new Date())
     		.toJobParameters();  
@@ -58,14 +60,16 @@ public class ApplicationWithJobLauncher {
         	
         	System.exit(0);
         	
-        } else {
+        } else if(NEW_EPISODES_NOTIFICATION_JOB.equals(args[0])){
         	JobParameters jobParameters = new JobParametersBuilder()
     		.addDate("date", new Date())
     		.addString("updateFrequency", args[1])
     		.toJobParameters();  
         	
-        	jobLauncher.run(ctx.getBean("newEpisodesNotificationJob",  Job.class), jobParameters);   
-        }   
+        	jobLauncher.run(ctx.getBean(NEW_EPISODES_NOTIFICATION_JOB,  Job.class), jobParameters);   
+        } else {
+        	throw new IllegalArgumentException("Please provide a valid Job name as first application parameter");
+        }
      
         System.exit(0);
     }
